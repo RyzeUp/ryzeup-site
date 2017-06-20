99f3d06b5c43eb74ccd4e3cf46a73cc91b35cceb
westinn <nicolaswestin@gmail.com>
2017-06-19 20:06:48 -0400

    Changes to service and controller

--- public/views/home/controllers/home.controller.client.js
+++ public/views/home/controllers/home.controller.client.js
@@ -6,10 +6,14 @@
         .module('RU')
         .controller('homeController', homeController);
 
-    function homeController($http) {
-
+    function homeController($http,
+                            congressService) {
         var model = this;
 
+        function init() {
+            congressService.senateReq()
+        };
+        init();
         $http.get('/api/congress/senate')
             .then(function (members) {
                 model.members = members.data.results[0].members;
